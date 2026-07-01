c6fe5dd4ff878e38a2b6b2056a65401204ae19ba
Logan DeBoer <lddeboer@cpp.edu>
2026-07-01 14:32:52 -0700

    organized code, added shape by sex

--- scripts/SPACES-workshop-HarperScript.R
+++ scripts/SPACES-workshop-HarperScript.R
@@ -1,12 +1,17 @@
 library(ggplot2)
 library(ratdat)
 
-ggplot(complete_old,aes(x =hindfoot_length, y= weight))+
+ggplot(complete_old,aes(x = hindfoot_length,
+                        y = weight))+
   geom_point()
 
-myplot <- ggplot(complete_old,aes(x =hindfoot_length, y= weight, color=species))+
-  geom_point(alpha = 0.2)
+myplot <- ggplot(complete_old,aes(x = hindfoot_length,
+                                  y = weight,
+                                  color=species,
+                                  shape = sex))+
+  geom_point(alpha = 0.2,)
 
 myplot+
-  theme_classic()
+  theme_minimal()
 myplot
+
